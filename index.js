2f9dd1b13e780a1d52fa8fa9178da1fd464fc38c
Shammah David <shammahdavid.sd@gmail.com>
2024-11-08 02:50:12 -0800

    Initialising Calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+// My Calculator Program
+
+const display = document.getElementById("screen")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Check your input bro"
+    }
+}
